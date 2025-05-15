4ab4e749e8cbd005d165dad6ac5ec4218019ca26
Helmiat <miasaritam@gmail.com>
2025-05-15 21:03:39 +0800

    Add files via upload

---
+++ meet6.py
@@ -0,0 +1,6 @@
+# Input umur dari pengguna
+umur = int(input("Masukkan umur Anda: "))
+
+# Menentukan kategori umur menggunakan ternary operator
+# Menampilkan hasil
+print(f"Anda termasuk {kategori}.")
